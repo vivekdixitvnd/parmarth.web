ab2376e2d2fee16bad9758feb4c135ba71db44b3
ScripticArnav <arnav.1807prof@gmail.com>
2025-05-09 21:40:35 +0530

    some changes fix

--- client/src/components/HelpCard/HelpCard.jsx
+++ client/src/components/HelpCard/HelpCard.jsx
@@ -1,41 +1,44 @@
-import React from "react";
 import styles from "./HelpCard.module.css";
+import { Link } from "react-router-dom";
 
 const HelpCard = ({
   src,
   title,
   description,
-  primaryBtnText = 'Donate Now',
-  secondaryBtnText = 'Learn More',
-  primaryBtnUrl = '',
-  secondaryBtnUrl = '',
-  titleColor = '#2a9be7',
-  dividerColor = '#2a9be7',
-  primaryBtnColor = '#277bc0',
-  secondaryBtnColor = '#2a9be7',
-  bgColor = 'rgba(253, 243, 228, 0.85)'
+  primaryBtnText = "Donate Now",
+  secondaryBtnText = "Learn More",
+  primaryBtnUrl = "",
+  secondaryBtnUrl = "",
+  titleColor = "#2a9be7",
+  dividerColor = "#2a9be7",
+  primaryBtnColor = "#277bc0",
+  secondaryBtnColor = "#2a9be7",
+  bgColor = "rgba(253, 243, 228, 0.85)",
 }) => {
   return (
-    <div className={styles.card} style={{
-      backgroundImage: `url(${src})`,
-      '--title-color': titleColor,
-      '--divider-color': dividerColor,
-      '--primary-btn-color': primaryBtnColor,
-      '--secondary-btn-color': secondaryBtnColor,
-      '--bg-color': bgColor
-    }}>
+    <div
+      className={styles.card}
+      style={{
+        backgroundImage: `url(${src})`,
+        "--title-color": titleColor,
+        "--divider-color": dividerColor,
+        "--primary-btn-color": primaryBtnColor,
+        "--secondary-btn-color": secondaryBtnColor,
+        "--bg-color": bgColor,
+      }}
+    >
       <div className={styles.contentOverlay}></div>
       <div className={styles.cardContent}>
         <h2 className={styles.title}>{title}</h2>
         <div className={styles.divider}></div>
         <p className={styles.description}>{description}</p>
         <div className={styles.buttonContainer}>
-        <a href={primaryBtnUrl} className={styles.actionButton}>
+          <Link to={primaryBtnUrl} className={styles.actionButton}>
             {primaryBtnText}
-          </a>
-          <a href={secondaryBtnUrl} className={styles.secondaryButton}>
+          </Link>
+          <Link to={secondaryBtnUrl} className={styles.secondaryButton}>
             {secondaryBtnText}
-          </a>
+          </Link>
         </div>
       </div>
     </div>
