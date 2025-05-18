aa901d12737543c473f58f5468f5677a5debead1
Parmarth2015 <parmarth@ietlucknow.ac.in>
2025-05-18 23:36:19 +0530

    Update attendance.js

--- backend/controllers/attendance.js
+++ backend/controllers/attendance.js
@@ -24,7 +24,7 @@ export const getAttendanceByDate = async (req, res) => {
     const { date } = req.params;
     const data = await Attendance.find({ date });
 
-    res.status(200).json(data);
+    res.status(200).json({ attendance: data });
   } catch (err) {
     res.status(500).json({ message: "Server error", error: err.message });
   }
