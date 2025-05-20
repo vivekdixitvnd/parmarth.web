aebb6e3693d80ae3c626adf6add7bcf285eb4f80
Parmarth2015 <parmarth@ietlucknow.ac.in>
2025-05-20 07:23:05 +0530

    Update StudyMaterial.routes.js

--- backend/routes/StudyMaterial.routes.js
+++ backend/routes/StudyMaterial.routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import materialController from '../controllers/StudyMaterial.js';
+import { uploadMaterial, getAllMaterials } from '../controllers/StudyMaterial.js';
 
 const router = express.Router();
 
