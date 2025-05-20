addd031ab72852401a7222385dc1dc273948e583
Parmarth2015 <parmarth@ietlucknow.ac.in>
2025-05-21 00:44:41 +0530

    Update StudyMaterial.routes.js

--- backend/routes/StudyMaterial.routes.js
+++ backend/routes/StudyMaterial.routes.js
@@ -1,12 +1,15 @@
-// ✅ FILE: routes/StudyMaterial.routes.js
-
 import express from 'express';
 import upload from '../config/cloudinary.js';
-import { uploadMaterial, getAllMaterials } from '../controllers/StudyMaterial.js';
+import {
+  uploadMaterial,
+  uploadMaterialBase64,
+  getAllMaterials
+} from '../controllers/StudyMaterial.js';
 
 const router = express.Router();
 
 router.post('/upload', upload.single('file'), uploadMaterial);
+router.post('/uploadBase64', uploadMaterialBase64);
 router.get('/all', getAllMaterials);
 
 export default router;
