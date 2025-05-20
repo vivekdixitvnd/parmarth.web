3700f9015e7facdfe1824ee3fa65e58fd563289f
Parmarth2015 <parmarth@ietlucknow.ac.in>
2025-05-20 11:47:20 +0530

    Update attendance.routes.js

--- backend/routes/attendance.routes.js
+++ backend/routes/attendance.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
+import isAuth from "../middleware/is-auth.js";
 import { getAttendanceByDate, markAttendance, getVolunteerAttendanceCount, upload } from "../controllers/attendance.js";
 
 const router = express.Router();
 
 router.post("/api/attendance", upload.array("photos", 7), markAttendance);
 router.get("/api/attendance/:date", getAttendanceByDate);
-router.get("/api/attendance/total", getVolunteerAttendanceCount);
+router.get("/api/attendance/total", isAuth, getVolunteerAttendanceCount);
 
 export default router;
