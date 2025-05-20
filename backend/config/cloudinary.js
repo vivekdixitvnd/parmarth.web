8ad9ef40fb45dea7032a96d65c81a1298c6a9264
Parmarth2015 <parmarth@ietlucknow.ac.in>
2025-05-20 15:14:08 +0530

    Create cloudinary.js

---
+++ backend/config/cloudinary.js
@@ -0,0 +1,26 @@
+// config/cloudinary.js
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import multer from 'multer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'my_uploads',
+    allowed_formats: ['jpg', 'png', 'pdf', 'docx'],
+    public_id: (req, file) => `${Date.now()}-${file.originalname}`,
+  },
+});
+
+const upload = multer({ storage });
+
+export default upload;
