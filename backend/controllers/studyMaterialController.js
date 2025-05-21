75b821ad3158b15be11c5efea479bc2ac81d8669
Parmarth2015 <parmarth@ietlucknow.ac.in>
2025-05-21 12:10:42 +0530

    Update studyMaterialController.js

--- backend/controllers/studyMaterialController.js
+++ backend/controllers/studyMaterialController.js
@@ -12,8 +12,9 @@ export const uploadStudyMaterial = async (req, res) => {
 
     // Upload to Cloudinary
     const result = await cloudinary.uploader.upload(req.file.path, {
-      resource_type: "auto",
+      resource_type: "raw",
       folder: "study_material",
+      access_mode: "public",
     });
 
     // Save to DB
